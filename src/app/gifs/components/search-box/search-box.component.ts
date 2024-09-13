20770f85e2c942cd8c977f4660f37249ba4754e2
cglidon <cglidon3d@gmail.com>
2024-09-13 20:08:10 +0200

    Estructura base

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-box',
+  templateUrl: './search-box.component.html',
+  styleUrl: './search-box.component.scss'
+})
+export class SearchBoxComponent implements OnInit {
+  
+  
+  
+  
+  
+  constructor(){
+  }
+  
+  
+  ngOnInit(): void {
+    
+  }
+
+}
---
+++ src/app/gifs/gifs.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomePagesComponent } from './pages/home-pages/home-pages.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { CardListComponent } from './components/card-list/card-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomePagesComponent,
+    SearchBoxComponent,
+    CardListComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    HomePagesComponent
+  ]
+})
+export class GifsModule { }
